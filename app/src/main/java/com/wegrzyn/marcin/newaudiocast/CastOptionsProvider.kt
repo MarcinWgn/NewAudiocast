0f8b8fb7ecf37906fd348235652038872b7c56c5
MarcinWgn <wireamg@gmail.com>
2022-03-19 20:49:42 +0100

    added notification options

--- app/src/main/java/com/wegrzyn/marcin/newaudiocast/CastOptionsProvider.kt
+++ app/src/main/java/com/wegrzyn/marcin/newaudiocast/CastOptionsProvider.kt
@@ -2,9 +2,12 @@ package com.wegrzyn.marcin.newaudiocast
 
 import android.content.Context
 import com.google.android.gms.cast.CastMediaControlIntent
+import com.google.android.gms.cast.LaunchOptions
 import com.google.android.gms.cast.framework.CastOptions
 import com.google.android.gms.cast.framework.OptionsProvider
 import com.google.android.gms.cast.framework.SessionProvider
+import com.google.android.gms.cast.framework.media.CastMediaOptions
+import com.google.android.gms.cast.framework.media.NotificationOptions
 
 
 class CastOptionsProvider : OptionsProvider {
@@ -12,8 +15,20 @@ class CastOptionsProvider : OptionsProvider {
 
     override fun getCastOptions(context: Context): CastOptions {
 
+        val notificationOptions = NotificationOptions.Builder()
+            .setTargetActivityClassName(MainActivity::class.java.name)
+            .build()
+
+        val castMediaOptions = CastMediaOptions.Builder()
+            .setNotificationOptions(notificationOptions)
+            .setMediaSessionEnabled(true)
+            .build()
+
+
         return CastOptions.Builder()
             .setReceiverApplicationId(CastMediaControlIntent.DEFAULT_MEDIA_RECEIVER_APPLICATION_ID)
+            .setCastMediaOptions(castMediaOptions)
+            .setStopReceiverApplicationWhenEndingSession(true)
             .build()
     }
 
