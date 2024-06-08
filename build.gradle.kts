47f64da3eaf5079acb803c50db542413abbab3f1
MarcinWgn <wireamg@gmail.com>
2024-06-08 18:03:13 +0200

    update to kotlin 2.0.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,5 +4,7 @@ plugins {
     alias(libs.plugins.androidLibrary) apply (false)
     alias(libs.plugins.kotlinAndroid) apply false
     alias(libs.plugins.ksp) apply false
+
+    alias(libs.plugins.compose.compiler) apply false
 }
 true
