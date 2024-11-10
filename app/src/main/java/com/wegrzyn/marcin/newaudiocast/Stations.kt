b3d45a8939e3e65bd37423ea9ef459c61f870dd8
MarcinWgn <wireamg@gmail.com>
2024-11-10 20:12:31 +0100

    update depen and url

--- app/src/main/java/com/wegrzyn/marcin/newaudiocast/Stations.kt
+++ app/src/main/java/com/wegrzyn/marcin/newaudiocast/Stations.kt
@@ -5,7 +5,7 @@ class Stations {
         val stationsList = arrayListOf(
             RadioStation("Nowy Swiat","https://stream.nowyswiat.online/mp3", "https://nowyswiat.online/wp-content/uploads/2020/09/fala-690x690.png", "https://nowyswiat.online/ramowka/"),
             RadioStation("Kraków","https://stream3.nadaje.com:9117/radiokrakow", "https://www.radiokrakow.pl/application/site/web/images/logo.png", "https://www.radiokrakow.pl/ramowka/"),
-            RadioStation("TOK FM","https://27823.live.streamtheworld.com/RADIO_TOKFM.mp3","https://bi.im-g.pl/im/2/22623/m22623812.png","https://audycje.tokfm.pl/ramowka"),
+            RadioStation("TOK FM","https://23623.live.streamtheworld.com/RADIO_TOKFM.mp3","https://bi.im-g.pl/im/2/22623/m22623812.png","https://audycje.tokfm.pl/ramowka"),
             RadioStation("RMF Classic","https://rs102-krk.rmfstream.pl/rmf_classic","https://www.rmfclassic.pl/static/frontend/images/logo-classic.png","https://www.rmfclassic.pl/radio/ramowka"),
             RadioStation("357","https://stream.rcs.revma.com/ye5kghkgcm0uv", "https://d9hwwxbc8j0dw.cloudfront.net/public/authors/399444/4a1eb7b0-b926-4018-a8d6-13b61878c90e.jpg", "https://radio357.pl/ramowka"),
             RadioStation("Manx","https://listen-manxradio.sharp-stream.com/manxradiofmmobile.aac?","https://mmo.aiircdn.com/61/60b78be49f831.png","https://www.manxradio.com/on-air/schedule/fm/"),
