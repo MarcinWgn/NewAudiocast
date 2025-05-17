577242f6b6501d2ce187a4281d0904635aa9c4b1
MarcinWgn <wireamg@gmail.com>
2025-05-17 22:32:25 +0200

    updated gradle and station img

--- app/src/main/java/com/wegrzyn/marcin/newaudiocast/Stations.kt
+++ app/src/main/java/com/wegrzyn/marcin/newaudiocast/Stations.kt
@@ -9,12 +9,12 @@ class Stations {
             RadioStation("RMF Classic","https://rs102-krk.rmfstream.pl/rmf_classic","https://www.rmfclassic.pl/static/frontend/images/logo-classic.png","https://www.rmfclassic.pl/radio/ramowka"),
             RadioStation("357","https://stream.rcs.revma.com/ye5kghkgcm0uv", "https://d9hwwxbc8j0dw.cloudfront.net/public/authors/399444/4a1eb7b0-b926-4018-a8d6-13b61878c90e.jpg", "https://radio357.pl/ramowka"),
             RadioStation("Manx","https://listen-manxradio.sharp-stream.com/manxradiofmmobile.aac?","https://mmo.aiircdn.com/61/60b78be49f831.png","https://www.manxradio.com/on-air/schedule/fm/"),
-            RadioStation("Trójka","http://stream3.polskieradio.pl:8904/;","https://trojka.polskieradio.pl/assets/birthday/logo_trojka.png","https://trojka.polskieradio.pl/ramowka"),
+            RadioStation("Trójka","http://stream3.polskieradio.pl:8904/;","https://trojka.polskieradio.pl/logo_trojka.svg","https://trojka.polskieradio.pl/ramowka"),
             RadioStation("Antyradio","https://27863.live.streamtheworld.com/ANTYRADIOAAC.aac?","https://gfx.antyradio.pl/design/antyradio/src/images/logo.png","https://www.antyradio.pl/radio/Ramowka"),
             RadioStation("Eska Rock","https://ic1.smcdn.pl/5380-1.mp3", "https://cdn.files.smcloud.net/t/logo.svg","https://www.eskarock.pl/program/"),
             RadioStation("RMF","http://31.192.216.3/rmf_fm","https://www.rmf.fm/assets/images/logo-bez-x2.png","https://www.rmf.fm/ramowka/"),
             RadioStation("ZET","http://zt01.cdn.eurozet.pl/zet-net.mp3","https://gfx.radiozet.pl/extension/radiozet-header-2/design/standard/images/logo--red.svg","https://player.radiozet.pl/Ramowka"),
-            RadioStation("Pogoda","https://radiostream.pl/tuba139-1.mp3","https://radiopogoda.pl/wp-content/themes/agora_wave/assets/rp/logoRP_kolor_a.png","https://radiopogoda.pl/ramowka")
+            RadioStation("Pogoda","https://radiostream.pl/tuba139-1.mp3","https://gfx.radiopogoda.pl/extension/radiozet-header-2/design/radiopogoda/images/logo-circle.svg","https://radiopogoda.pl/ramowka")
         )
     }
 }
