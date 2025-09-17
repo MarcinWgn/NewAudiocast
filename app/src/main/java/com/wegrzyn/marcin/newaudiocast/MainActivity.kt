ae7d176f7cb587768bbb765390c6f9bfa0811402
MarcinWgn <wireamg@gmail.com>
2025-09-18 00:30:01 +0200

    removed XML ActionBar

--- app/src/main/java/com/wegrzyn/marcin/newaudiocast/MainActivity.kt
+++ app/src/main/java/com/wegrzyn/marcin/newaudiocast/MainActivity.kt
@@ -3,22 +3,24 @@ package com.wegrzyn.marcin.newaudiocast
 import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
-import android.net.Uri
 import android.os.Bundle
 import android.view.Gravity
-import android.view.Menu
 import android.widget.Toast
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.safeDrawingPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
@@ -40,15 +42,16 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
-import androidx.compose.ui.platform.ComposeView
 import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.viewinterop.AndroidView
 import androidx.constraintlayout.compose.ConstraintLayout
-import androidx.core.content.ContextCompat.startActivity
+import androidx.core.net.toUri
 import androidx.lifecycle.viewmodel.compose.viewModel
+import androidx.mediarouter.app.MediaRouteButton
 import coil.compose.AsyncImage
 import coil.compose.rememberAsyncImagePainter
 import coil.decode.SvgDecoder
@@ -60,64 +63,71 @@ class MainActivity : AppCompatActivity() {
     private val vm: MainViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
+        enableEdgeToEdge()
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        val composeView = findViewById<ComposeView>(R.id.compose_layout_id)
-        composeView.setContent {
+        setContent {
             AppTheme {
-                when (LocalConfiguration.current.orientation) {
-                    Configuration.ORIENTATION_PORTRAIT -> PortraitView()
-                    Configuration.ORIENTATION_LANDSCAPE -> LandscapeView()
-                    else -> PortraitView()
+                Box(modifier = Modifier.safeDrawingPadding()) {
+                    when (LocalConfiguration.current.orientation) {
+                        Configuration.ORIENTATION_PORTRAIT -> PortraitView()
+                        Configuration.ORIENTATION_LANDSCAPE -> LandscapeView()
+                        else -> PortraitView()
+                    }
                 }
             }
         }
     }
 
-    override fun onCreateOptionsMenu(menu: Menu): Boolean {
-        super.onCreateOptionsMenu(menu)
-
-        menuInflater.inflate(R.menu.main_menu, menu)
-        CastButtonFactory.setUpMediaRouteButton(
-            applicationContext,
-            menu,
-            R.id.media_route_menu_item
-        )
-        return true
-    }
 
     @Composable
     fun PortraitView() {
-        Scaffold(bottomBar = {
-            ControlBelt(modifier = Modifier)
-        }) {
+        Scaffold(
+            bottomBar = {
+                ControlBelt(modifier = Modifier)
+            }) {
             CardsList(
                 modifier = Modifier
-                    .fillMaxSize()
-                    .padding(top = 8.dp, bottom = it.calculateBottomPadding())
+                    .fillMaxHeight()
+                    .padding(bottom = it.calculateBottomPadding())
             )
+
         }
+    }
+
+    // TODO: cast button
+    @Composable
+    fun CastButtonXmlView() {
+        AndroidView(factory = { context ->
+            val castButton = MediaRouteButton(context)
+            CastButtonFactory.setUpMediaRouteButton(context, castButton)
+
+            return@AndroidView castButton
+        }, update = {
+
+        })
     }
 
     @Composable
     fun LandscapeView() {
         ConstraintLayout {
             val (list, belt) = createRefs()
-            CardsList(modifier = Modifier
-                .fillMaxWidth(0.6f)
-                .constrainAs(list) {
-                    end.linkTo(parent.end)
-                    top.linkTo(parent.top)
-                    bottom.linkTo(parent.bottom)
-                })
-            ControlBelt(modifier = Modifier
-                .fillMaxWidth(0.4f)
-                .constrainAs(belt) {
-                    start.linkTo(parent.start)
-                    end.linkTo(list.start)
-                    bottom.linkTo(parent.bottom)
-                })
+            CardsList(
+                modifier = Modifier
+                    .fillMaxWidth(0.6f)
+                    .constrainAs(list) {
+                        end.linkTo(parent.end)
+                        top.linkTo(parent.top)
+                        bottom.linkTo(parent.bottom)
+                    })
+            ControlBelt(
+                modifier = Modifier
+                    .fillMaxWidth(0.4f)
+                    .constrainAs(belt) {
+                        start.linkTo(parent.start)
+                        end.linkTo(list.start)
+                        bottom.linkTo(parent.bottom)
+                    })
         }
     }
 
@@ -144,19 +154,20 @@ class MainActivity : AppCompatActivity() {
             colors = CardDefaults.cardColors(Color.Transparent),
             border = CardDefaults.outlinedCardBorder(true)
         ) {
-            Column(modifier = Modifier
-                .fillMaxWidth()
-                .clickable {
-                    model.radioCast(radioStation = station, toast = {
-                        val tst = Toast.makeText(
-                            ctx,
-                            "Moja Kochana Jadziu CASTUJ :)",
-                            Toast.LENGTH_SHORT
-                        )
-                        tst.setGravity(Gravity.CENTER, 0, 0)
-                        tst.show()
-                    })
-                }) {
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .clickable {
+                        model.radioCast(radioStation = station, toast = {
+                            val tst = Toast.makeText(
+                                ctx,
+                                "Moja Kochana Jadziu CASTUJ :)",
+                                Toast.LENGTH_SHORT
+                            )
+                            tst.setGravity(Gravity.CENTER, 0, 0)
+                            tst.show()
+                        })
+                    }) {
                 Text(
                     style = AppTypography.titleLarge, text = station.name, modifier = Modifier
                         .padding(start = 16.dp, top = 8.dp, bottom = 8.dp, end = 16.dp)
@@ -195,8 +206,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun goPage(uriString: String, context: Context) {
-        val uri = Uri.parse(uriString)
-        startActivity(context, Intent(Intent.ACTION_VIEW, uri), null)
+        val uri = uriString.toUri()
+        context.startActivity(Intent(Intent.ACTION_VIEW, uri), null)
     }
 
 
@@ -208,82 +219,68 @@ class MainActivity : AppCompatActivity() {
         val isShowing by model.beltIsShowing.observeAsState(false)
         val castDevName by model.castDevName.observeAsState("")
 
-        if (isShowing) {
-            Card(
-                modifier = modifier
-                    .fillMaxWidth(1f)
-                    .padding(start = 8.dp, end = 8.dp, bottom = 8.dp),
-                shape = RoundedCornerShape(20.dp),
-                colors = CardDefaults.cardColors(Color.Transparent),
-                border = CardDefaults.outlinedCardBorder(true)
-            ) {
 
-                ConstraintLayout(modifier = Modifier.fillMaxWidth(1f)) {
-                    val (image, text, button, progress, device) = createRefs()
-                    Image(painter = rememberAsyncImagePainter(
-                        ImageRequest.Builder(LocalContext.current)
-                            .data(data = imgUri)
-                            .placeholder(R.drawable.baseline_radio_24)
-                            .decoderFactory(SvgDecoder.Factory())
-                            .crossfade(500)
-                            .build()
-                    ),
+        Card(
+            modifier = modifier
+                .fillMaxWidth()
+                .padding(start = 8.dp, end = 8.dp, bottom = 8.dp),
+            shape = RoundedCornerShape(20.dp),
+            colors = CardDefaults.cardColors(Color.Transparent),
+            border = CardDefaults.outlinedCardBorder(true)
+        ) {
+
+            Row(modifier = Modifier.fillMaxWidth(), verticalAlignment = Alignment.CenterVertically) {
+                if (isShowing) Row(verticalAlignment = Alignment.CenterVertically) {
+                    Image(
+                        painter = rememberAsyncImagePainter(
+                            ImageRequest.Builder(LocalContext.current)
+                                .data(data = imgUri)
+                                .placeholder(R.drawable.baseline_radio_24)
+                                .decoderFactory(SvgDecoder.Factory())
+                                .crossfade(500)
+                                .build()
+                        ),
                         contentDescription = null,
                         modifier = Modifier
-                            .size(50.dp)
+                            .size(60.dp)
                             .clip(CircleShape)
-                            .constrainAs(image) {
-                                start.linkTo(parent.start, margin = 16.dp)
-                                top.linkTo(parent.top)
-                                bottom.linkTo(parent.bottom)
-                            })
-                    Text(
-                        style = AppTypography.titleLarge, modifier = Modifier
-                            .padding(16.dp)
-                            .constrainAs(text) {
-                                start.linkTo(image.end)
-                                bottom.linkTo(device.top)
-                                top.linkTo(parent.top)
-                            }, text = name
+                            .padding(start = 16.dp)
                     )
-                    Text(
-                        style = AppTypography.titleSmall, modifier = Modifier
-                            .constrainAs(device) {
-                                start.linkTo(image.end, margin = 16.dp)
-                                bottom.linkTo(parent.bottom, margin = 16.dp)
-                                top.linkTo(text.bottom)
-                            }, text = castDevName
-                    )
-                    IconButton(onClick = { model.playPause() }, modifier = Modifier
-                        .constrainAs(button) {
-                            top.linkTo(parent.top, margin = 16.dp)
-                            end.linkTo(parent.end, margin = 16.dp)
-                            bottom.linkTo(parent.bottom, margin = 16.dp)
-                        }) {
+                    Column(modifier = Modifier.padding(start = 24.dp, end = 50.dp)) {
+                        Text(style = AppTypography.titleLarge, modifier = Modifier, text = name)
+                        Text(style = AppTypography.titleSmall, text = castDevName)
+                    }
+                    IconButton(
+                        onClick = { model.playPause() }, modifier = Modifier.padding(start = 16.dp)
+                    ) {
                         if (value == MainViewModel.PLAYING || value == MainViewModel.PAUSE) {
                             Icon(
                                 imageVector =
-                                when (value) {
-                                    MainViewModel.PLAYING -> ImageVector.vectorResource(id = R.drawable.pause_24)
-                                    MainViewModel.PAUSE -> ImageVector.vectorResource(id = R.drawable.play_24)
-                                    else -> ImageVector.vectorResource(id = R.drawable.play_24)
-                                }, contentDescription = null, modifier = Modifier
+                                    when (value) {
+                                        MainViewModel.PLAYING -> ImageVector.vectorResource(id = R.drawable.pause_24)
+                                        MainViewModel.PAUSE -> ImageVector.vectorResource(id = R.drawable.play_24)
+                                        else -> ImageVector.vectorResource(id = R.drawable.play_24)
+                                    }, contentDescription = null, modifier = Modifier
                                     .padding(8.dp)
-                                    .size(35.dp)
+                                    .size(40.dp)
                                     .clip(CircleShape)
                             )
                         }
                     }
+
                     if (value == MainViewModel.BUFFERING) {
-                        CircularProgressIndicator(modifier = Modifier
-                            .constrainAs(progress) {
-                                top.linkTo(parent.top, margin = 16.dp)
-                                end.linkTo(parent.end, margin = 16.dp)
-                                bottom.linkTo(parent.bottom, margin = 16.dp)
-                            })
+                        CircularProgressIndicator()
                     }
+                }
+                Row(
+                    modifier = Modifier.fillMaxWidth().padding(end = 16.dp),
+                    horizontalArrangement = Arrangement.End,
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    CastButtonXmlView()
                 }
             }
         }
     }
 }
+
